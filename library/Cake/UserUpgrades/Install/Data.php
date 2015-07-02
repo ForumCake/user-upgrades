934bb0cb7099d9f014118b64c7540de58a561b1d
Jonathan Wainwright <jon@waindigo.org>
2015-07-02 15:53:58 +0100

    Alpha 2

--- library/Cake/UserUpgrades/Install/Data.php
+++ library/Cake/UserUpgrades/Install/Data.php
@@ -1,16 +1,6 @@
 <?php
 namespace Cake\UserUpgrades;
 
-use Cake\Install_DataAbstract;
-
-class Install_Data extends Install_DataAbstract
+class Install_Data extends \Cake\Install_DataAbstract
 {
-
-    public function getModules()
-    {
-        return array(
-            'PayPalIpnRules' => true,
-            'SalesTax' => true
-        );
-    }
 }
---
+++ library/Cake/UserUpgrades/Proxy.php
@@ -0,0 +1,6 @@
+<?php
+namespace Cake\UserUpgrades;
+
+class Proxy extends \Cake\Proxy
+{
+}
